7d802c876d5889e82ab34f7a0eebfd9acfec46a5
techguruCD <colinkong0406@yahoo.com>
2024-05-24 05:21:16 -0700

    -

---
+++ TemplateBackend/TemplateBackend/src/Application/Features/ApplicationUsers/Commands/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,38 @@
+﻿using TemplateBackend.Application.Common.Models;
+using TemplateBackend.Application.Contracts;
+﻿using TemplateBackend.Application.Contracts;
+using TemplateBackend.Domain.Entities;
+using Microsoft.Rest;
+
+namespace TemplateBackend.Application.Features.ApplicationUsers.Commands.DeleteUser;
+
+public class DeleteUserRequest : IRequest<Result>
+{
+    public string Email { get; set; }
+}
+
+public class DeleteUserCommand: IRequestHandler<DeleteUserRequest, Result>
+{
+    private readonly IApplicationUsersRepository _applicationUsersRepository;
+
+    public DeleteUserCommand(
+        IApplicationUsersRepository applicationUsersRepository)
+    {
+        _applicationUsersRepository = applicationUsersRepository;
+    }
+
+    public async Task<Result> Handle(DeleteUserRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var user = await _applicationUsersRepository.GetUser(request.Email);
+            if (user == null) return Result.Failure(["User not found"]);
+            var result = await _applicationUsersRepository.DeleteUser(request.Email);
+            return Result.Success(result);
+        }
+        catch (Exception)
+        {
+            return Result.Failure(["Failed to delete user"]);
+        }
+    }
+}
---
+++ TemplateBackend/TemplateBackend/src/Application/Features/ApplicationUsers/Commands/Login/LoginCommand.cs
@@ -0,0 +1,65 @@
+﻿using TemplateBackend.Application.Common.Models;
+using TemplateBackend.Application.Contracts;
+using TemplateBackend.Domain.Entities;
+
+namespace TemplateBackend.Application.Features.ApplicationUsers.Commands.Login;
+
+public class LoginRequest : IRequest<Result>
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public bool RememberMe { get; set; }
+}
+
+public class LoginCommand : IRequestHandler<LoginRequest, Result>
+{
+    private readonly IApplicationUsersRepository _applicationUsersRepository;
+    private readonly IJWTManagerRepository _jwtManagerRepository;
+
+
+    public LoginCommand(
+        IApplicationUsersRepository applicationUsersRepository,
+        IJWTManagerRepository jwtManagerRepository)
+    {
+        _applicationUsersRepository = applicationUsersRepository;
+        _jwtManagerRepository = jwtManagerRepository;
+    }
+
+    public async Task<Result> Handle(LoginRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var user = await _applicationUsersRepository.GetUser(request.Username);
+
+            if (user == null)
+            {
+                return Result.Failure(["User with this email does not exist."]);
+            }
+            
+            var userWithCredentialsExists = await _applicationUsersRepository.IsValidUserAsync(request.Username, request.Password);
+
+            if (!userWithCredentialsExists) return Result.Failure(["Wrong credentials."]);
+
+            var token = _jwtManagerRepository.GenerateToken(request.Username);
+            if (token == null)
+            {
+                return Result.Failure(["Failed to generate jwt token."]);
+            }
+
+            UserRefreshTokens userRefreshToken = new()
+            {
+                RefreshToken = token.RefreshToken,
+                Email = user.Email
+            };
+            _applicationUsersRepository.AddUserRefreshTokens(userRefreshToken);
+
+            return Result.Success(token);         
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+
+    }
+
+}
